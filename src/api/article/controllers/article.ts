54aef412a4555e03e712909012432ad0a4134907
yesdopepe <laiskashkash33@gmail.com>
2025-02-14 01:09:02 +0300

    created backend

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -1,7 +1,7 @@
 /**
- *  article controller
+ * article controller
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from '@strapi/strapi'
 
 export default factories.createCoreController('api::article.article');
---
+++ src/api/expirience/controllers/expirience.ts
@@ -0,0 +1,7 @@
+/**
+ * expirience controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::expirience.expirience');
